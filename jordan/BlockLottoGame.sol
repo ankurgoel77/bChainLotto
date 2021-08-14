2df7fdf72af9e0095179df3e590bc76b562aff84
Jordan Hickerson <hickersonjordan@gmail.com>
2021-08-14 13:42:46 -0500

    blocklottogame.sol added, needs randomnumpick fx

---
+++ jordan/BlockLottoGame.sol
@@ -0,0 +1,108 @@
+pragma solidity ^0.5.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/drafts/Counters.sol"
+
+contract BlockLottoGame{
+    using Counters for Counters.Counter;
+    Counters.Counter ticketids;
+    address starter;
+    address beneficiary; // charity gets all if nobody wins :-) 
+    
+    // closingTime
+    bool isOpen;
+    uint lottoPot;
+    
+    
+    struct Ticket {
+        address payable buyer;
+        uint num1;
+        uint num2;
+        uint num3;
+        uint num4;
+        uint num5;
+        uint num6;
+    
+    }
+    mapping(uint => Ticket) public tickets;
+    
+    event ticketBought (address buyer, uint num1, uint num2, uint num3, uint num4, uint num5, uint num6);
+    event winningNumbers(uint num1, uint num2, uint num3, uint num4, uint num5, uint num6)
+    event winningTicket(address buyer, uint payout);
+    
+    constructor(
+        address payable _beneficiary,
+        uint maxBallNum
+        // uint _closingTime 
+        ) public {
+            
+        starter = msg.sender;
+        beneficiary = _beneficiary; 
+        maxBallNum = _maxBallNum;
+        isOpen = true;
+        // closingTime = _closingTime
+    }
+    modifier openStatus{
+        require (isOpen, "Lotto already closed");
+        _;
+    }
+    
+    function buyTicket(uint num1, uint num2, uint num3, uint num4, uint num5, uint num6) public payable openStatus{
+        // buy 1 ETH ticket (return excess), store & emit address+ nums, & add 1ETH to pot  
+        require (msg.value >= 1 ether, "SEND MORE");
+        // require now <= closingTime, "closed"
+        if (msg.value > 1 ether) {
+            msg.sender.transfer(msg.value - 1 ether);
+        }
+        lottoPot += 1 ether;
+        Ticket ticket = new ticket(msg.sender, num1, num2, num3, num4, num5, num6);
+        ticketids.increment();
+        tickets[ticketids.current()] = ticket
+        
+        //@TODO: verify all nums are 1 - maxBallNum
+        emit ticketBought(msg.sender, num1, num2, num3, num4, num5, num6);
+    }
+    
+    //@TODO insert function to pick random winning nums
+    
+    function finalize()public openStatus{
+        // pick winning nums, find winning tickets, divide .8 pot among winners
+        require (msg.sender == starter, "Only the starter can close the lottery");
+        isOpen = false;
+        
+        Counters.Counter winnerIds;
+        uint winningAmount = 0;
+        mapping (uint ==> Ticket) winners;
+        // require for closingTime;
+        //@TODO call function to pick random winning nums
+        // emit winningNumbers(num1, num2, num3, num4, num5, num6);
+        
+        
+        for (uint i=1; i<= ticketids; i++) {
+            Ticket currentTicket = tickets[i];
+            if (currentTicket.num1 == num1 && currentTicket.num2 == num2 && currentTicket.num3 == num3 && currentTicket.num4 == num4 && currentTicket.num5 == num5 && currentTicket.num6 == num6 ) {
+                winnerIds.increment();
+                winners[winnerIds.current()] = currentTicket;
+            }
+        }
+        if (winnerIds.current() > 0) {
+            winningAmount = lottoPot * .8 / winners.current();
+        }
+        
+        for (uint i = 1; i <= winnerIds.current(); i++) {
+            winners[i].buyer.transfer(winningAmount);
+            emit winningTicket(buyer, winningAmount);
+        }
+        
+        lottoPot -= winningAmount // takes care of remainders
+        beneficiary.transfer(lottoPot)
+        
+    }
+    
+    //@TODO: <maybe> rollover lotto (sees previous pot >>>>to potential next contract) 
+    
+    
+    // uint256 public lotteryId;
+    // function startLotto(uint256 duration){}
+    
+    
+}
