997d035993cacd8b03796e6cf8c4e3f47f989617
Jordan Hickerson <hickersonjordan@gmail.com>
2021-08-19 21:23:15 -0500

    finished cli for user & player, need to debug issue for beneficiary on creator and test player

--- jordan/cli_creator.py
+++ jordan/cli_creator.py
@@ -1,6 +1,9 @@
-# Creator
+import json
+from web3 import Web3
 
-creator_account
+from eth_account import Account
+
+# Creator
 """
 
 initiate the contract with all required parameters: 
@@ -11,9 +14,61 @@ initiate the contract with all required parameters:
 
 """
 
-def main():
-    """
-    
-    """
+f = open("abi.json")
+abi = json.load(f)
+f.close()
 
-main()
+f = open("bytecode.json")
+bytecode_json = json.load(f)
+f.close()
+bytecode = bytecode_json["object"]
+
+ganache_server = "http://127.0.0.1:8545"
+web3 = Web3(Web3.HTTPProvider(ganache_server))
+web3.isConnected()
+
+pvtkey = input("type in your private key: ")
+creator_account = Account.from_key(pvtkey)
+beneficiary = str(input("type in beneficiary address: "))
+
+web3.eth.default_account = creator_account
+def init_game():
+    lotto_contract = web3.eth.contract(abi=abi, bytecode=bytecode)
+
+    tx_hash = lotto_contract.constructor(beneficiary, 60).transact()
+    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+    return tx_receipt.contractAddress # need to give this address to players
+
+def get_lottopot(contractAddress):
+    current_contract = web3.eth.contract(address=contractAddress, abi=abi)
+    lottoPot = current_contract.functions.lottoPot().call()
+    return lottoPot
+
+def get_winningnums(contractAddress):
+    current_contract = web3.eth.contract(address=contractAddress, abi=abi)
+    winning_numbers = current_contract.functions.getWinningNumbers().call() 
+    return winning_numbers
+
+def finalize(contractAddress):
+    current_contract = web3.eth.contract(address=contractAddress, abi=abi)
+    tx_hash = current_contract.functions.finalize().transact()
+    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+
+def main():
+    choice = input("construct game? Y or N ")
+    if choice == "Y":
+        contractAddress = init_game()
+        print(f"Contract address for players , {contractAddress}")
+        while choice != "Q":
+            choice = input("Menu === b for Balance, w for WinningNums, f for Finalize, Q for quit")
+            if choice == "b":
+                print(get_lottopot(contractAddress))
+            elif choice == "w":
+                print(get_winningnums(contractAddress))
+            elif choice == "f":
+                finalize(contractAddress)
+
+
+
+if __name__ == "__main__":
+   main()
--- jordan/cli_player.py
+++ jordan/cli_player.py
@@ -1,10 +1,24 @@
 import json
 from web3 import Web3
+
+from eth_account import Account
 import random
 
 # User
 
-user_account = "user account address or privKeytoAccount"
+f = open("abi.json")
+abi = json.load(f)
+f.close()
+
+ganache_server = "http://127.0.0.1:8545"
+web3 = Web3(Web3.HTTPProvider(ganache_server))
+web3.isConnected()
+
+pvtkey = input("type in your private key: ")
+user_account = Account.from_key(pvtkey)
+contractAddress = input("type in contract address: ")
+contract = web3.eth.contract(address=contractAddress, abi=abi)
+web3.eth.default_account = user_account
 
 def pick6():
     """
@@ -21,13 +35,49 @@ def pick6():
 
 
 
-def buyTicket(six_numbers_str):
+def buyTicket(six_numbers):
     """
-    Input 6 numbers as a string separated by spaces, can alternatively call the pick6 function here
+    Input 6 numbers separated by spaces, can alternatively call the pick6 function here
     """
+    tx_hash = contract.functions.buyTicket(
+        six_numbers[0],
+        six_numbers[1],
+        six_numbers[2],
+        six_numbers[3],
+        six_numbers[4], 
+        six_numbers[5]).transact({"value": web3.toWei(1, "ether")})
+    
+    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
+
+def main():
+    choice = input("buy a ticket? Y or N ")
+    if choice == "Y":
+        choice = input("want to pick your own numbers? Y or N ")
+        if choice == "Y":
+            chosen_nums = input("enter 6 numbers 1-60 separated by spaces: ")
+            chosen_nums = chosen_nums.split(" ")
+            chosen_nums = [int(i) for i in chosen_nums]
+            buyTicket(chosen_nums)
+        elif choice == "N":
+            buyTicket(pick6())
+    if choice == "N":
+        choice = "Menu === w for get winning numbers, L for check lotto pot "
+        if choice == "w":
+            isFinished = contract.functions.isFinished().call()
+            if isFinished:
+                print(f"The winning numbers are: {contract.functions.getWinningNumbers().call()}")
+            else:
+                print("Winning numbers are not available until the lotto has been drawn.")
+
+        elif choice == "L":
+            print(f"Current Pot: {contract.functions.lottoPot().call()}")
 
 
 
+
+
+if __name__ == "__main__":
+   main()
 
 
 
