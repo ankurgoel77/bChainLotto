c7e4d57fc37142aad114b65e567b24c5fbe16d9e
Ankur Goel <akgoel@hardymachine.com>
2021-08-21 13:50:02 -0500

    poker can be deployed

--- ankur/jstest/main.js
+++ ankur/jstest/main.js
@@ -1,7 +1,3 @@
-function multiply(num1, num2) {
-    let result = num1 * num2;
-    return result;
-}
 
 function getAddress() {
     const txt_pvtkey = document.querySelector('#pvtkey');
@@ -25,16 +21,67 @@ function constructGame() {
     let player = document.querySelector("#acc_address").value;
 
     let gasPrice = web3.eth.gasPrice;
-    let gasPriceHex = web3.toHex(gasPrice);
-    let gasLimitHex = web3.toHex(6000000000000);
+    let gasPriceHex = web3.utils.toHex(gasPrice);
+    let gasLimitHex = web3.utils.toHex(6000000000000);
     let block = web3.eth.getBlock("latest");
     let nonce = web3.eth.getTransactionCount(dealer_account.address, "pending");
-    let nonceHex = web3.toHex(nonce);
+    let nonceHex = web3.utils.toHex(nonce);
 
-    let contractData = '0x'+ bytecode["object"];
+    let contractData = null;
 
-    let blackjackContract = web3.eth.contract(abi);
+    // contractData = blackjackContract.new.getData(player, betAmount, {
+    //     data: '0x' + bytecode["object"]
+    // });  
+
+    let blackjackContract = new web3.eth.Contract(abi);
+    blackjackContract.options.data = '0x'+bytecode["object"];
+
+    blackjackContract.deploy({arguments: [player, String(betAmount)]
+    }).send({
+        from: dealer_account.address,
+        gasLimit : 6721975,
+        value : "251"
+    }).then(function(newContractInstance){
+        console.log(newContractInstance.options.address);
+        currentContract = new web3.eth.Contract(abi, newContractInstance.options.address )
+    });
+
+
     
+
+    let rawTx = {
+        nonce : nonceHex,
+        gasPrice : gasPriceHex,
+        gasLimit : gasLimitHex,
+        data: contractData,
+        from : dealer_account.account,
+        value : "251"
+    }
+
+    web3.eth.accounts.signTransaction(rawTx,dealer_pvt_key).then(console.log, console.log)
+    
+}
+
+function ante() {
+    let player = document.querySelector("#acc_address").value;
+    currentContract.methods.ante().send({
+        from : player,
+        value : "100",
+        gasLimit : 6721975,
+    }).then(function(receipt) {
+        currentContract.methods.getPlayerHand().call({from: player}).then(function(value) {
+            document.querySelector('#player_string_hand').innerHTML = hand_to_str(value);
+            document.querySelector('#player_card_hand').innerHTML = hand_to_unicode(value);
+            currentContract.methods.getDealerHand().call({from:player}).then(function(value){
+                document.querySelector('#dealer_card_hand').innerHTML = hand_to_unicode(value);
+            })
+        })
+    } );
+}
+
+function hit() {
+    let player = document.querySelector("#acc_address").value;
+    let 
 }
 
 function num_to_unicode(number) {
@@ -123,6 +170,53 @@ function hand_to_str(hand) {
     return value.join(', ');
 }
 
+function nums_to_value(hand){
+    let baseValue = 0
+    let cardValue = 0
+    let acesCount = 0
+
+    for (let i = 0; i < hand.length, i++) {
+        cardValue = i % 13;
+        if (cardValue > 0){ // #not an ace
+            if (cardValue < 10) {
+                baseValue += cardValue + 1;
+            } else {
+                baseValue += 10;
+            }
+        } else {
+            acesCount += 1;
+        }
+    }
+
+    if (acesCount == 1) {
+        if (baseValue + 11 <= 21) {
+            return baseValue + 11;
+        } else {
+            return baseValue + 1;
+        }
+    } else if (acesCount == 2) {
+        if (baseValue + 11 <= 21) {
+            return baseValue + 12;
+        } else {
+            return baseValue + 2;
+        }
+    } else if (acesCount == 3){
+        if (baseValue + 11 <= 21) {
+            return baseValue + 13;
+        } else {
+            return baseValue + 3;
+        }
+    } else if (acesCount == 4) {
+        if (baseValue + 11 <= 21) {
+            return baseValue + 14;
+        } else {
+            return baseValue + 4;
+        }
+    } else {
+        return baseValue;
+    }
+}
+
 const provider = new Web3.providers.HttpProvider('http://127.0.0.1:8545');
 const web3 = new Web3(provider);
 
@@ -132,17 +226,19 @@ const web3 = new Web3(provider);
 let dealer_pvt_key = "d5046127ca371f85b9268b4c3b6a2b5fa891c66e38c2532726215a7ce4673d32";
 let dealer_account = web3.eth.accounts.privateKeyToAccount(dealer_pvt_key);
 
+let currentContract = null;
 
 
-const pCard = document.querySelector('#card');
-let myString = num_to_unicode(20);
-pCard.innerHTML = myString;
 
-let pHand = [23,45,9,50];
-document.querySelector('#player_string_hand').innerHTML = hand_to_str(pHand);
-document.querySelector('#player_card_hand').innerHTML = hand_to_unicode(pHand);
+// const pCard = document.querySelector('#card');
+// let myString = num_to_unicode(20);
+// pCard.innerHTML = myString;
 
-document.querySelector("#btn_start").disabled = false;
+// let pHand = [23,45,9,50];
+// document.querySelector('#player_string_hand').innerHTML = hand_to_str(pHand);
+// document.querySelector('#player_card_hand').innerHTML = hand_to_unicode(pHand);
+
+// document.querySelector("#btn_start").disabled = false;
 
 // const myHeading = document.querySelector('h1');
 // myHeading.textContent = "Hello World!";
