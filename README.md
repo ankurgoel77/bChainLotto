2c0fad9f93b60b6f7b555c8b159e08d99bacba0c
Ankur Goel <akgoel@hardymachine.com>
2021-08-25 08:49:43 -0500

    starting readme

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # bChainLotto
-Capstone project for FinTech Bootcamp
+Capstone project for FinTech Bootcamp  
+by Quang Le, Jordan Hickerson, Ankur Goel
+
+# Introduction and Purpose
+
+# Setting Up a Public Ganache Testnet
+
+# Lotto Smart Contract
+
+# Lotto Python User Interface
+
+# BlackJack Smart Contract
+
+# BlackJack Web User Interface
--- ankur/init_bchain.py
+++ ankur/init_bchain.py
@@ -1,12 +1,13 @@
 from web3 import Web3
 import csv
+import sys
 
 csvpath = "bip39_addresses.csv"
 ganache = ganache = "http://45.33.17.146:8545"
 web3 = Web3(Web3.HTTPProvider(ganache))
 if not web3.isConnected() :
     print("not connected")
-    exit()
+    sys.exit()
 
 with open(csvpath, "r") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
@@ -27,6 +28,6 @@ with open(csvpath, "r") as csvfile:
         }
         signed_tx = web3.eth.account.sign_transaction(tx, main_private_key)
         tx_hash = web3.eth.send_raw_transaction(signed_tx.rawTransaction)
-        print(f"transerred 2 ETH to wallet {current_address} with tx hash {web3.toHex(tx_hash)}")
+        print(f"transerred 100 ETH to wallet {current_address} with tx hash {web3.toHex(tx_hash)}")
 
     
